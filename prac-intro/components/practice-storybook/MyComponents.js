ee1f8bc0e178792d7c6b7e00b8b511f37550cb48
course-k <course.k.ky@gmail.com>
2024-03-05 06:26:05 +0900

    Controlタブの学習

--- prac-intro/components/practice-storybook/MyComponents.js
+++ prac-intro/components/practice-storybook/MyComponents.js
@@ -1,5 +1,10 @@
+
 const MyComponents = ({
-    isNewUser = false
+    isNewUser, bool, num1, num2
+    , obj1, obj2, enum1, enum2
+    , enum3, enum4, enum5, enum6
+    , str1, str2, str3
+    , handleClick
 }) => {
     return (
         <>
@@ -7,7 +12,21 @@ const MyComponents = ({
             <input type="text" id="userId" disabled={isNewUser} />
             <label htmlFor="password" >パスワード：</label>
             <input type="password" id="password" disabled={isNewUser} />
-            <button type="button" >{isNewUser ? '新規登録' : 'ログイン'}</button>
+            <button type="button" onClick={handleClick}>{isNewUser ? '新規登録' : 'ログイン'}</button>
+            <h3>真偽値：{bool}</h3>
+            <h3>数値1: {num1}</h3>
+            <h3>数値2: {num2}</h3>
+            <h3>オブジェクト1: {obj1}</h3>
+            <h3>オブジェクト2: {obj2}</h3>
+            <h3>列挙1: {enum1}</h3>
+            <h3>列挙2: {enum2}</h3>
+            <h3>列挙3: {enum3}</h3>
+            <h3>列挙4: {enum4}</h3>
+            <h3>列挙5: {enum5}</h3>
+            <h3>列挙6: {enum6}</h3>
+            <h3>文字列1: {str1}</h3>
+            <h3>文字列2: {str2}</h3>
+            <h3>文字列3: {str3}</h3>
         </>
     );
 };
--- prac-intro/stories/MyComponents.stories.js
+++ prac-intro/stories/MyComponents.stories.js
@@ -1,12 +1,108 @@
+import { actionAsyncStorage } from "next/dist/client/components/action-async-storage.external";
 import MyComponents from "../components/practice-storybook/MyComponents";
 
 export default {
     title: 'MyApp/MyComponents',
-    component: MyComponents
+    component: MyComponents,
+    argTypes: {
+        bool: {
+            type: 'boolean',
+            description: '真偽値を設定'
+        }
+        , num1: {
+            type: 'number',
+            description: '数値1を設定'
+        }
+        , num2: {
+            type: 'number',
+            description: '数値2を設定',
+            control: { type: 'range' }
+        }
+        , obj1: {
+            type: {
+                name: 'object',
+                value: { prop: 'hoge' }
+            },
+            description: 'オブジェクト1を設定',
+            control: { type: 'object' }
+        }
+        , obj2: {
+            type: {
+                name: 'object',
+                value: { prop: 'hoge' }
+            },
+            description: 'オブジェクト2を設定',
+            control: { type: 'file' }
+        }
+        , enum1: {
+            type: {
+                name: 'enum',
+                value: ['apple', 'banana', 'cherry']
+            },
+            description: '列挙1を設定',
+            control: { type: 'radio' }
+        }
+        , enum2: {
+            type: {
+                name: 'enum',
+                value: ['apple', 'banana', 'cherry']
+            },
+            description: '列挙2を設定',
+            control: { type: 'inline-radio' }
+        }
+        , enum3: {
+            type: {
+                name: 'enum',
+                value: ['apple', 'banana', 'cherry']
+            },
+            description: '列挙3を設定',
+            control: { type: 'check' }
+        }
+        , enum4: {
+            type: {
+                name: 'enum',
+                value: ['apple', 'banana', 'cherry']
+            },
+            description: '列挙4を設定',
+            control: { type: 'inline-check' }
+        }
+        , enum5: {
+            type: {
+                name: 'enum',
+                value: ['apple', 'banana', 'cherry']
+            },
+            description: '列挙5を設定',
+            control: { type: 'select' }
+        }
+        , enum6: {
+            type: {
+                name: 'enum',
+                value: ['apple', 'banana', 'cherry']
+            },
+            description: '列挙6を設定',
+            control: { type: 'multi-select' }
+        }
+        , str1: {
+            type: 'string',
+            description: '文字列1を設定'
+        }
+        , str2: {
+            type: 'string',
+            description: '文字列2を設定',
+            control: { type: 'color' }
+        }
+        , str3: {
+            type: 'string',
+            description: '文字列3を設定',
+            control: { type: 'date' }
+        }
+    }
 }
 
 export const NewUser = {
-    render: () => <MyComponents isNewUser={true} />
+    args: {
+        handleClick: action('true')
+    }
 }
 
 
