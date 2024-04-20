d7402c339ce0e6ec80e05a51f5245b8717c6c7f4
course-k <course.k.ky@gmail.com>
2024-04-20 16:14:43 +0900

    management status

--- practice-formik-front/src/index.js
+++ practice-formik-front/src/index.js
@@ -1,27 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useFormik } from "formik";
 import "./index.css";
 
 const SimpleForm = () => {
+  /* fomikの定義 */
+  const { values, handleChange } = useFormik({
+    /* 初期値の定義 */
+    initialValues: {
+      fullName: "",
+      email: "",
+    },
+  });
+
+  /* submit時の挙動の定義（仮） */
+  const printFormik = (e) => {
+    e.preventDefault();
+    console.log(`fullName : ${values.fullName}`);
+    console.log(`email : ${values.email}`);
+  };
+
   return (
     <>
-      <form className="form-container">
+      {/* onSubmitの追加 */}
+      <form className="form-container" onSubmit={printFormik}>
         <div className="form-group">
           <label htmlFor="fullName">full name:</label>
+          {/* onChangeとvalueの追加 */}
           <input
             type="text"
             id="fullName"
             name="fullName"
             placeholder="your name"
+            onChange={handleChange}
+            value={values.fullName}
           />
         </div>
         <div className="form-group">
           <label htmlFor="email">email address:</label>
+          {/* onChangeとvalueの追加 */}
           <input
             type="email"
             id="email"
             name="email"
             placeholder="your email-address"
+            onChange={handleChange}
+            value={values.email}
           />
         </div>
         <button type="submit" className="submit-button">
