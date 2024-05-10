cb1d2fd8700eced34d6e255218dd87a3942c4c32
course-k <course.k.ky@gmail.com>
2024-05-11 03:16:26 +0900

    practice generics

--- for-qiita-ts/src/app.ts
+++ for-qiita-ts/src/app.ts
@@ -1,3 +1,19 @@
-let arr: string[] = ["apple", "banana"];
-arr.push("orange");
-arr.push(100);
+interface Lengthwise {
+  length: number;
+}
+
+function getLength<T extends Lengthwise>(obj: T): number {
+  return obj.length;
+}
+
+const str = "Hello";
+console.log(getLength(str)); // 5
+
+const arr = [1, 2, 3];
+console.log(getLength(arr)); // 3
+
+const obj = { length: 10 };
+console.log(getLength(obj)); // 10
+
+const num = 42;
+console.log(getLength(num)); // エラー
